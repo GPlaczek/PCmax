5406f372ffe17e7f014b6ca4de96808b15b0bb8f
Grzegorz Płaczek <g01placzek@gmail.com>
2021-10-09 15:53:25 +0200

    Added generic min heap class

---
+++ MinHeap.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class MinHeap{
+    T *array;
+    int size;
+    void heapify(int i){
+        while(2*i+1 < size){
+            if(2*i+2 < size && array[2*i+2] < array[2*i+1] && array[2*i+2] < array[i]){
+                swap(array[i], array[2*i+2]);
+                i=2*i+2;
+            }else if(array[2*i+1] <= array[i]){
+                swap(array[i], array[2*i+1]);
+                i=2*i+1;
+            }else break;
+        }
+    }
+public:
+    MinHeap(int size, T *array){
+        this -> array = array;
+        this -> size = size;
+        for(int i = size/2 - 1; i >= 0; i--){
+            heapify(i);
+        }
+    }
+    void readHeap(){
+        for(int i = 0; i < size; i++){
+            cout << array[i] << " ";
+        }
+        cout << endl;
+    }
+};
