4b2ccc3547376c23647589f1cc67ebb01244cc0d
Kamil <kamil271e@gmail.com>
2021-10-28 12:53:59 +0200

    brute force (do poprawki)

--- lib/PCmax.hpp
+++ lib/PCmax.hpp
@@ -8,4 +8,5 @@ public:
     static vector<int> *populate(int, int, int*, int);
     static int genetic(int, int, int*);
     static int greedy(int, int, int*);
+    static int brute(int, int, int*);
 };
